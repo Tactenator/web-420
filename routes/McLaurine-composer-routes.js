30714de75df86f91a2ecd13602bbb9eeba6fe52d
Tactenator <trevor.mclaurine@gmail.com>
2023-06-13 10:45:26 -0500

    YAML comments added

--- routes/McLaurine-composer-routes.js
+++ routes/McLaurine-composer-routes.js
@@ -40,6 +40,24 @@ router.get('/composers', async (req,res) => {
     }
 })
 
+/**
+ * findComposerById
+ * @openapi
+ * /composers/:id:
+ *   get:
+ *     tags:
+ *       - Composers
+ *     description: Returns a specific composer designated by the user input. The composer is retrieved by grabbing an id from the url parameters.
+ *     summary: Returns the data for a specific composer.
+ *     operationid: findComposerById
+ *     responses:
+ *       '200':
+ *         description: "Successful retrieval of a document containing the composer"
+ *       '500':
+ *         description: "Server exceptions"
+ *       '501':
+ *         description: "MongoDB exceptions"
+ */
 router.get('/composers/:id', async (req, res) => {
     try {
         Composer.findOne({'_id':req.params.id}, function(err, composers) {
@@ -59,6 +77,37 @@ router.get('/composers/:id', async (req, res) => {
     }
 })
 
+/**
+ * createComposer
+ * @openapi
+ * /composers:
+ *   post:
+ *     tags:
+ *       - Composers
+ *     operationid: createComposer
+ *     description: Creates and places a new composer the composer API database.
+ *     summary: Creates a new composer
+ *     requestBody:
+ *       description: A user input describing the Composer object
+ *       content:
+ *         application/x-www-form-urlencoded:
+ *           schema:
+ *             type: object
+ *             schema:
+ *               firstName:
+ *                 type: string
+ *                 required: true
+ *               lastName:
+ *                 type: string
+ *                 required: true
+ *     responses:
+ *       '200':
+ *         description: Successful creation and posting of a new composer to the composer API
+ *       '500':
+ *         description: Server Exception
+ *       '501':
+ *         description: MongoDB Exception
+ */
 router.post('/composers', async (req, res) => {
     const newComposer = {
         firstName: req.body.firstName,
