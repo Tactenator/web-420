ecd1dc01fac7a606f765be7b1b33031a5c60644f
Trevor McLaurine <trevor.mclaurine@gmail.com>
2023-06-15 10:15:49 -0500

    still working on routes

--- app.js
+++ app.js
@@ -28,6 +28,14 @@ const options = {
 
 const openapiSpecification = swaggerJSDoc(options);
 
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+    });
+
 app.use('/api-docs', swaggerUIExpress.serve, swaggerUIExpress.setup(openapiSpecification));
 app.use('/composers', composerRoutes)
 
