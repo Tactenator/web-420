59ec469a20077384350587440143921b5f9e7df9
Trevor McLaurine <trevor.mclaurine@gmail.com>
2023-06-28 09:18:18 -0500

    adding signup week 6

--- routes/McLaurine-session-routes.js
+++ routes/McLaurine-session-routes.js
@@ -15,8 +15,32 @@ const saltRounds = 10;
 
 const router = express.Router();
 
+async function findUser (user) {
+    let userFound = User.findOne({ 'userName': user })
+    return userFound
+}
+
 router.post('/signup', async (req, res) => {
     //do signup stuff
+    let newUser = {
+        user: req.body.userName,
+        password: req.body.password,
+        email: req.body.email
+    }
+    try {
+        let user = findUser(newUser.user)
+
+        if(!user) {
+            await User.create(newUser)
+            res.status(200).json(newUser)
+        }
+        else {
+            res.status(401).send({ 'message': `MongoDB Exception: 501`})
+        }
+    }
+    catch(e) {
+        res.status(500).send({ 'message': `Server Exception: ${e.message}`})
+    }
 })
 
 router.post('/login', async (req, res) => {
